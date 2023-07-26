12699eb920f99d87e2cbeeec156884b504d334e1
Frederico Balieiro <fredericoahb@gmail.com>
2023-07-25 22:42:40 -0300

    first commit

---
+++ backend/src/main/java/com/appmeta/appmeta/AppmetaApplication.java
@@ -0,0 +1,13 @@
+package com.appmeta.appmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/appmeta/appmeta/AppmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appmeta.appmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
